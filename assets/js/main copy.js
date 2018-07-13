19ba7b80e6d94613e6825115fd05e5bca7472eb9
tuanbinh <ntb.cs2@gmail.com>
2018-07-13 22:28:02 +0700

    Update profile

--- assets/js/main copy.js
+++ assets/js/main copy.js
@@ -55,11 +55,11 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "anandpurushottam");
+    GitHubCalendar("#github-graph", "tuanbinhb");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "anandpurushottam", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "tuanbinhb", selector: "#ghfeed" });
 
 
 });
